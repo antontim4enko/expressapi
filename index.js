7689e685dd24a928eecbf0c802fe1a25e5156ee5
Den <33726252+antontim4enko@users.noreply.github.com>
2019-02-02 12:20:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -98,4 +98,4 @@ app.use("/api", router);
 
 
 // launch our backend into a port
-app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+app.listen(process.env.PORT || API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
